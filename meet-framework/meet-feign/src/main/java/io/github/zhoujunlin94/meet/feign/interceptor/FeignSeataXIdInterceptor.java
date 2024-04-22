f03f632ce965362a6123d8321d9a236103b76cd1
zhoujunlin <zhoujunlin@gaodun.com>
2024-04-22 17:21:09 +0800

    deploy 1.1.0

--- meet-framework/meet-feign/src/main/java/io/github/zhoujunlin94/meet/feign/interceptor/FeignSeataXIdInterceptor.java
+++ meet-framework/meet-feign/src/main/java/io/github/zhoujunlin94/meet/feign/interceptor/FeignSeataXIdInterceptor.java
@@ -1,25 +1,40 @@
 package io.github.zhoujunlin94.meet.feign.interceptor;
 
+import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import io.seata.core.context.RootContext;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
 
 /**
  * @author zhoujunlin
  * @date 2023年09月23日 15:47
  */
+@Slf4j
 @Component
-@ConditionalOnClass(RootContext.class)
+@ConditionalOnClass(name = {FeignSeataXIdInterceptor.ROOT_CONTEXT_CLAZZ})
 public class FeignSeataXIdInterceptor implements RequestInterceptor {
+
+    public static final String ROOT_CONTEXT_CLAZZ = "io.seata.core.context.RootContext";
+
+    private static final String XID = "TX_XID";
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
-        String xid = RootContext.getXID();
-        if (StrUtil.isNotBlank(xid)) {
-            requestTemplate.header(RootContext.KEY_XID, xid);
+        try {
+            // 获取XID
+            Method getXIDMethod = ReflectUtil.getMethodByName(Class.forName(ROOT_CONTEXT_CLAZZ), "getXID");
+            String xid = ReflectUtil.invokeStatic(getXIDMethod);
+            if (StrUtil.isNotBlank(xid)) {
+                requestTemplate.header(XID, xid);
+            }
+        } catch (Exception e) {
+            log.error("FeignSeataXIdInterceptor传递XID出错", e);
         }
     }
+
 }
