c6b200f31792826813c7d62e5a39d8b5c8ca7697
zhoujunlin <@WSXcft6>
2025-11-24 14:29:49 +0800

    3.2.4

--- meet-framework/meet-mybatis_plus/src/test/java/io/github/zhoujunlin94/meet/mybatis_plus/test/MeetUserTest.java
+++ meet-framework/meet-mybatis_plus/src/test/java/io/github/zhoujunlin94/meet/mybatis_plus/test/MeetUserTest.java
@@ -1,11 +1,14 @@
 package io.github.zhoujunlin94.meet.mybatis_plus.test;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import io.github.zhoujunlin94.meet.mybatis_plus.MeetMybatisPlusApplication;
 import io.github.zhoujunlin94.meet.mybatis_plus.entity.meet.MeetUser;
 import io.github.zhoujunlin94.meet.mybatis_plus.repository.MeetUserRepository;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.MDC;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -24,6 +27,7 @@ public class MeetUserTest {
 
     @Test
     public void testMeetUserMapper() {
+        MDC.put("X-REQUEST-ID", "123456");
         MeetUser meetUser = meetUserRepository.getById(1);
         log.info("meetUser: {}", meetUser);
     }
@@ -33,6 +37,12 @@ public class MeetUserTest {
     public void testAll() {
         List<MeetUser> meetUsers = meetUserRepository.list();
         log.info("meetUsers: {}", meetUsers);
+    }
+
+    @Test
+    public void testPage() {
+        PageInfo<MeetUser> entityPageInfo = PageHelper.startPage(2, 2).doSelectPageInfo(() -> meetUserRepository.list());
+        log.warn("entityPageInfo: {}", entityPageInfo);
     }
 
 
