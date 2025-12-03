2d356c8d967f0621c65d524c12ceb1c08ada13f3
zhoujunlin <@WSXcft6>
2025-12-03 16:59:37 +0800

    3.2.4

--- meet-framework/meet-mybatis_plus/src/main/java/io/github/zhoujunlin94/meet/mybatis_plus/repository/BaseRepository.java
+++ meet-framework/meet-mybatis_plus/src/main/java/io/github/zhoujunlin94/meet/mybatis_plus/repository/BaseRepository.java
@@ -2,7 +2,9 @@ package io.github.zhoujunlin94.meet.mybatis_plus.repository;
 
 import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.repository.CrudRepository;
 import com.google.common.collect.Lists;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,8 +17,12 @@ import java.util.List;
  */
 public class BaseRepository<M extends BaseMapper<T>, T> extends CrudRepository<M, T> {
 
-    protected LambdaQueryWrapper<T> currentLambdaQueryWrapper() {
-        return new LambdaQueryWrapper<T>();
+    protected LambdaQueryWrapper<T> lambdaQueryWrapper() {
+        return Wrappers.lambdaQuery(getEntityClass());
+    }
+
+    protected LambdaUpdateWrapper<T> lambdaUpdateWrapper() {
+        return Wrappers.lambdaUpdate(getEntityClass());
     }
 
     protected void excludeSelect(LambdaQueryWrapper<T> lambdaQueryWrapper, List<String> properties) {
